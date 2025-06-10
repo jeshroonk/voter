5f9f48003d3bcdd349fe8847012befed8e9faab1
jeshroonk <jeshroonkatapur2005@gmail.com>
2025-06-11 05:27:40 +0530

    voter eligiballity file

---
+++ Voter.java
@@ -0,0 +1,12 @@
+public class Voter{
+    public static void main(String[]args){
+        int age=20;
+        if(age>=18){
+            System.out.println("you are eligibal to vote");
+        }else{
+            System.out.println("not eligibal to vote");
+        }
+    }
+
+}
+
