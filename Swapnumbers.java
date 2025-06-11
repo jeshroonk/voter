42f55a069ef684e06ee7b7ac3bd50107d4365a55
jeshroonk <jeshroonkatapur2005@gmail.com>
2025-06-11 05:49:09 +0530

    new

---
+++ Swapnumbers.java
@@ -0,0 +1,11 @@
+public class Swapnumbers{
+    public static void main(String[]args){
+        int a=20,b=10,temp;
+     System.out.println("Before swapping numbers: a = " + a + ", b = " + b);
+     temp=a;
+     a=b;
+     b=temp;
+     System.out.println("after swapping numbers: a = " + a + ", b = " + b);
+
+    }
+}
